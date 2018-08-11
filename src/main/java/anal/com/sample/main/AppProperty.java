91b6cb0da26a4ff1c4ab62c4670bef5821144921
arturnalepa <artur.nalepa@yahoo.pl>
2018-08-11 18:50:48 +0200

    nic szczegolnego

---
+++ src/main/java/anal/com/sample/main/AppProperty.java
@@ -0,0 +1,11 @@
+package anal.com.sample.main;
+import com.sun.xml.internal.fastinfoset.sax.Properties;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+
+public class AppProperty {
+
+}
